f86ae514f89b7aec55e3d2262dec15793df82aba
Cavidan Abdullayev <146195294+cavidan456@users.noreply.github.com>
2023-10-23 14:02:58 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,111 @@
+// 1 ci sualin helli
+
+// let a = 1;
+
+// let b = 4;
+
+// let c = 4;
+
+// let D = b**2 - 4*a*c;
+// if (D > 0) {
+//   let x1 = ((-b + Math.sqrt(D)) / 2) * a
+//   let x2 = ((-b - Math.sqrt(D)) / 2) * a
+//   console.log("x in 1 ci deyeri" + " " + x1)
+//   console.log("x in 2 ci deyeri" + " " + x2)
+// } else if (D < 0) {
+//   console.log("x in heqiqi koku yoxdur");
+// } else {
+//   let x1 = ((-b + Math.sqrt(D)) / 2) * a;
+//   let x2 = x1;
+//   console.log("x in sadece 1 heqiqi koku var" + " " + x1);
+// }
+
+// sual 2 helli
+
+// a = 19
+
+// for (let i = 0; i <= 1000; i++) {
+//     if (i % a  == 0) {
+//         document.writeln(i)
+//     }
+// }
+
+// sual 3-7 sert
+const arr = [2, 4, 5, 9, 1];
+
+// sual 3 helli
+// let min = arr[0]
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] < min ){
+//       min = arr[i];
+//     };
+// };
+
+// console.log(min);
+
+// sual 4 helli
+// let max = arr[0]
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > max ){
+//       max = arr[i];
+//     };
+// };
+
+// console.log(max);
+
+// sual 5 helli
+// let min = arr[0];
+// let minIndex = 0;
+
+// for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] < min) {
+//         min = arr[i];
+//         minIndex = i;
+//     }
+// }
+
+// console.log("en kiçik eded:", min);
+// console.log("en kiçik ededin indeksi: ", minIndex);
+
+// sual 6 helli
+
+// let max = arr[0];
+// let maxIndex = 0;
+
+// for (let i = 1; i < arr.length; i++) {
+//   if (arr[i] > max) {
+//     max = arr[i];
+//     maxIndex = i;
+//   }
+// }
+
+// console.log(maxIndex);
+
+
+// sual 7 helli
+// let cem = 0
+// for (let i = 0; i < arr.length; i++) {
+//     if (i % 2 == 1) {
+//         cem += arr[i]
+//     }
+// }
+// console.log(cem);
+
+// sual 8 helli
+//  let txt = "Mən Code Academy Tələbəsiyəm."
+//  let kicikHerf = txt.toLowerCase();
+//  console.log(kicikHerf);
+
+// sual 9 helli
+// let txt = "            Mən Code Academydə Programing tədrisi alıram            ";
+// console.log(txt.trim());
+
+// sual 10 helli
+// let txt = "Mən Code Academy Tələbəsiyəm."
+// console.log(txt.split(" "));
+
+// sual 11 helli
+let question11 =  "            Mən Code Academydə Programing tədrisi alıram            "
+let ansver = question11.trim();
+console.log(ansver.length);
